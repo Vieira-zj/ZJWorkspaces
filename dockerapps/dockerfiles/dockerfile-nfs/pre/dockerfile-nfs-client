81fdfdf9236ab160dea7961af8c91177a1ac3e4e
zhengjin <zivieira@163.com>
2018-07-04 00:31:25 +0800

    Add webapp and docker app files.

---
+++ dockerapps/dockerfiles/dockerfile-nfs/pre/dockerfile-nfs-client
@@ -0,0 +1,9 @@
+FROM ansible/ubuntu14.04-ansible
+
+# created at 2018-06-06
+MAINTAINER zivieira@163.com
+
+RUN apt-get update && apt-get install -y nfs-common && mkdir /mnt/nfstest
+
+CMD while true; do echo 'hello'; sleep 2; done;
+
---
+++ webapps/my_nginx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x -e
+
+file_conf="${HOME}/Workspaces/webapps/nginx_c.conf"
+
+if [[ ($1 =~ "stop") ]]; then
+    nginx -s stop
+    exit 0
+fi
+
+# note: "test" cannot use for customized conf.
+if [[ ($1 =~ "test") ]]; then
+    nginx -t ${file_conf}
+    exit 0
+fi
+
+nginx -v 
+nginx -c ${file_conf}
+
+set +x +e
