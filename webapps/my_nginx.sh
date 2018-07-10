7c770fd2b6460e2808ca2d831baede5a9920b9c9
zhengjin <zivieira@163.com>
2018-07-10 19:34:51 +0800

    Update nginx configs.

--- webapps/my_nginx.sh
+++ webapps/my_nginx.sh
@@ -3,6 +3,11 @@ set -x -e
 
 file_conf="${HOME}/Workspaces/ZJWorkspaces/webapps/nginx_c.conf"
 
+if [[ ($1 =~ "reload") ]]; then
+    nginx -s reload
+    exit 0
+fi
+
 if [[ ($1 =~ "stop") ]]; then
     nginx -s stop
     exit 0
