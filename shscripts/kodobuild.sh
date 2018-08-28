63f5ff5c47fc2466c63fedeced188cc24fff919b
zhengjin <zivieira@163.com>
2018-08-28 14:35:44 +0800

    update.

--- shscripts/kodobuild.sh
+++ shscripts/kodobuild.sh
@@ -10,15 +10,18 @@ source $QBOXROOT/base/env.sh
 #make
 
 # build app
-bin_dir="${HOME}/Downloads/tmp_files/bin"
-bin_name="bucketmigrate"
-bin_file="${bin_dir}/${bin_name}"
+#bin_dir="${HOME}/Downloads/tmp_files/bin"
+#bin_name="bucketmigrate"
+#bin_path="${bin_dir}/${bin_name}"
 
 #GOOS=linux GOARCH=amd64 go build qiniu.com/kodo/io/app/qboxio
 #GOOS=linux GOARCH=amd64 go build qiniu.com/kodo/kfile/app/qboxkfile
 
-#GOOS=linux GOARCH=amd64 go build -o ${bin_file} qiniu.com/kodo/tools/bucketmigrate
-go build -o ${bin_file} qiniu.com/kodo/tools/bucketmigrate
+#GOOS=linux GOARCH=amd64 go build -o $GOBIN/regionstat qiniu.com/kodo/rs2/roll/app/regionstat 
+#GOOS=linux GOARCH=amd64 go build -o $GOBIN/qboxftp qiniu.com/kodo/kfile/app/qboxftp
+
+#GOOS=linux GOARCH=amd64 go build -o ${bin_path} qiniu.com/kodo/tools/${bin_name}
+go build -o ${bin_path} qiniu.com/kodo/tools/${bin_name}
 
 set +x
 
