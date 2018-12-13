7b38ad47b478358ff39e1fddcf075d7c98bf71c3
zhengjin <zhengjin@163.com>
2018-12-13 16:37:48 +0800

    Add mac backup sh scripts.

---
+++ shscripts/mac_backups/zj_sh_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+# ex1, sub str
+tmp_str="hello world"
+echo "sub str: ${tmp_str:0:5}"
+echo "length: ${#tmp_str}"
+
+
+# ex2, trim last char
+func_trim_last_char() {
+    input1=$1
+    len=${#input1}
+    end=$((len-1))
+    echo ${input1:0:${end}}
+}
+
+ret=$(func_trim_last_char "testt")
+echo "trim last char results: ${ret}"
+
+
+set +ex
