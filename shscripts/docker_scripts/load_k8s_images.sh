6a0737a5872071c2cae6a1cdcb70ae7a78eeb797
zhengjin <zivieira@163.com>
2019-02-19 20:51:58 +0800

    Update load images script.

--- shscripts/docker_scripts/load_k8s_images.sh
+++ shscripts/docker_scripts/load_k8s_images.sh
@@ -1,20 +1,36 @@
 #/bin/bash
 set -ex
- 
-file="./images.v2.properties"
- 
-if [ -f "$file" ]; then
-  echo "$file found."
- 
-  while IFS='=' read -r key value
-  do
-    echo "${key}=${value}"
-    docker pull ${value}
-    docker tag ${value} ${key}
-    docker rmi ${value}
-  done < "$file"
-else
-  echo "$file not found."
-fi
+
+load_images_v1() {
+  images=(kube-apiserver:v1.1.13.3 kube-controller-manager:v1.13.3 kube-scheduler:v1.13.3 kube-proxy:v1.13.3 pause:3.1 etcd:3.2.24 coredns:1.2.6)
+  for ima in ${images[@]}; do
+    echo "pull image $ima"
+    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$ima
+    docker tag registry.cn-shenzhen.aliyuncs.com/google_containers/$ima k8s.gcr.io/$ima
+    docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$ima
+  done
+}
+
+load_images_v2() {
+  file="./images.v2.properties"
+   
+  if [ -f "$file" ]; then
+    echo "$file found."
+   
+    while IFS='=' read -r key value
+    do
+      echo "${key}=${value}"
+      docker pull ${value}
+      docker tag ${value} ${key}
+      docker rmi ${value}
+    done < "$file"
+  else
+    echo "$file not found."
+  fi
+}
+
+#load_images_v1
+load_images_v2
+echo "load docker images done."
 
 set +ex
