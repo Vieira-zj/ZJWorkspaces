2f34b055e2263ea4e025368627daf348b9bc6833
zhengjin <zivieira@163.com>
2019-02-21 15:53:16 +0800

    Add minikube hello demo deploy scripts.

--- shscripts/docker_scripts/load_k8s_images.sh
+++ shscripts/docker_scripts/load_k8s_images.sh
@@ -1,20 +1,26 @@
 #/bin/bash
 set -ex
 
-load_images_v1() {
+load_images_by_args() {
   images=(kube-apiserver:v1.1.13.3 kube-controller-manager:v1.13.3 kube-scheduler:v1.13.3 kube-proxy:v1.13.3 pause:3.1 etcd:3.2.24 coredns:1.2.6)
+  if [ -n $1 ]; then
+    images=($1)
+  fi
+
   for ima in ${images[@]}; do
     echo "pull image $ima"
-    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$ima
-    docker tag registry.cn-shenzhen.aliyuncs.com/google_containers/$ima k8s.gcr.io/$ima
+    if [ -z $DOCKER_CERT_PATH ]; then  # pull image only if local docker is running
+      docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$ima
+    fi
+    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$ima k8s.gcr.io/$ima
     docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$ima
   done
 }
 
-load_images_v2() {
-  file="./images.v2.properties"
+load_images_by_configs() {
+  file="./images.properties"
    
-  if [ -f "$file" ]; then
+  if [ -f $file ]; then
     echo "$file found."
    
     while IFS='=' read -r key value
@@ -29,8 +35,8 @@ load_images_v2() {
   fi
 }
 
-#load_images_v1
-load_images_v2
+load_images_by_args $1
+load_images_by_configs
 echo "load docker images done."
 
 set +ex
