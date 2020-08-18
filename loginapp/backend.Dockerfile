2abe08d8f94686b02046bd101ea19e51bfbb3b3e
zhengjin <jin.zheng@shopee.com>
2020-08-18 19:21:57 +0800

    Update deploy script.

--- loginapp/backend.Dockerfile
+++ loginapp/backend.Dockerfile
@@ -2,7 +2,7 @@
 #
 FROM rackspacedot/python37
 
-# WORKDIR /app
+WORKDIR /app
 # ADD ./login_backend /app
 
 RUN pip install flask
@@ -11,4 +11,5 @@ EXPOSE 12340
 ENV MYSQL localhost
 ENV FLASK_ENV test
 
-CMD ["/bin/bash", "-c", "while true; do echo 'hello'; sleep 10; done;"]
+CMD ["/bin/bash", "-c", "while true; do echo 'hello'; sleep 10; done"]
+# CMD ["/bin/bash", "-c", "python app.py"]
--- loginapp/run.sh
+++ loginapp/run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -eux
 
+function cp_app() {
+  target="${HOME}/Downloads/tmps/docker_vols/backend"
+  if [[ -d $target ]]; then
+    rm -rf $target
+  fi
+  cp -r login_backend/ $target	
+}
+
+function cp_dist() {
+  # docker run --name frontend -d --rm -p 8080:80 -v ${HOME}/Downloads/tmps/docker_vols/conf.d:/etc/nginx/conf.d nginx
+  docker cp login_frontend/dist/. frontend:/usr/share/nginx/html
+  docker cp login.conf frontend:/etc/nginx/conf.d/default.conf
+  docker exec frontend nginx -s reload
+}
+
 # build backend image and test
 if [[ $1 == "build" ]]; then
   docker build -f backend.Dockerfile -t login-backend:v1 .
@@ -8,25 +23,26 @@ if [[ $1 == "build" ]]; then
 fi
 
 if [[ $1 == "cp_app" ]]; then
-  target="${HOME}/Downloads/tmps/docker_vols/backend"
-  if [[ -d $target ]]; then
-    rm -rf $target
-  fi
-  cp -r login_backend/ $target
+  cp_app
 fi
 
 if [[ $1 == "cp_dist" ]]; then
-  # docker run --name proxy -d --rm -p 8080:80 nginx
-  docker cp "login_frontend/dist/." "proxy:/usr/share/nginx/html"
+  cp_dist
 fi
 
 if [[ $1 == "up" ]]; then
   docker-compose -f login-demo-compose.yaml up -d
-  # login url: http://logindemo.zj.com:8080/index.html
 fi
 
 if [[ $1 == "down" ]]; then
   docker-compose -f login-demo-compose.yaml down
+fi
+
+if [[ $1 == "main" ]]; then
+  # login url: http://logindemo.zj.com:8080
+  cp_app
+  docker-compose -f login-demo-compose.yaml up -d
+  cp_dist
 fi
 
 echo "done"
