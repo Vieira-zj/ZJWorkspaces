3d815d57bcf3650a9955aaba3d538960369404c4
zhengjin <zivieira@163.com>
2020-11-13 19:13:39 +0800

    Update clear unused goccenter addrs sh.

--- shscripts/common_scripts/clear_goc_addrs.sh
+++ shscripts/common_scripts/clear_goc_addrs.sh
@@ -3,6 +3,7 @@ set -eu
 
 server_name='is-topup-server'
 center_addr='http://goccenter.airpay-qa-testdev.i.test.airpay.in.th'
+# goc='/home/ld-sgdev/jin_zheng/bins/goc'
 
 function goc_list_addrs {
     goc list --center=${center_addr} | jq .
@@ -26,7 +27,21 @@ function goc_remove_addrs {
     rm ${tmp_file}
 }
 
-# goc_list_addrs
-goc_remove_addrs
+function clear_unused_addrs {
+    local items=$(goc list --center=${center_addr} |sed 's/{//g' |sed 's/}//g' |awk -F ',' '{for (i=1; i <= NF; i++) print $i;}')
+    for item in ${items}; do
+        name=$(echo $item |awk -F ':' '{print $1}' |sed 's/"//g')
+        echo "check service ${name}"
+        count=$(kubectl get pods -A |grep ${name} |wc -l)
+        if [[ ${count} -eq 0 ]]; then
+            echo "remove service ${name} from goccenter"
+            goc remove --center=${center_addr} --service=${name}
+        fi
+    done
+}
 
-echo "done"
+goc_list_addrs
+# goc_remove_addrs
+# clear_unused_addrs
+
+echo "Done"
