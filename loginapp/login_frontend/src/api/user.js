1c34418f9327efb3253de3b251faf20e4738de5a
zhengjin <zivieira@163.com>
2021-01-18 16:29:08 +0800

    Fix login app frontend.

--- loginapp/login_frontend/src/api/user.js
+++ loginapp/login_frontend/src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function apiLogin (data) {
   return request({
-    url: process.env.VUE_APP_BASE_API + '/login',
+    url: '/login',
     method: 'post',
     data
   })
@@ -10,23 +10,23 @@ export function apiLogin (data) {
 
 export function apiGetUser (name) {
   return request({
-    url: process.env.VUE_APP_BASE_API + "/getuser",
-    method: "get",
+    url: '/getuser',
+    method: 'get',
     params: { name }
   })
 }
 
 export function apiGetUsers (data) {
   return request({
-    url: process.env.VUE_APP_BASE_API + "/getusers",
-    method: "post",
+    url: '/getusers',
+    method: 'post',
     data
   })
 }
 
 export function apiNewUser (data) {
   return request({
-    url: process.env.VUE_APP_BASE_API + '/newuser',
+    url: '/newuser',
     method: 'post',
     data
   })
@@ -34,15 +34,15 @@ export function apiNewUser (data) {
 
 export function apiEditUser (data) {
   return request({
-    url: process.env.VUE_APP_BASE_API + "/edituser",
-    method: "post",
+    url: '/edituser',
+    method: 'post',
     data
   })
 }
 
 export function apiIsSuperUser () {
   return request({
-    url: process.env.VUE_APP_BASE_API + "/issuperuser",
-    method: "get"
+    url: '/issuperuser',
+    method: 'get'
   })
 }
